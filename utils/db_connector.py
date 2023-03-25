79d209f0af78148c50a61707017478bfdea9cff3
Params10 <params704@gmail.com>
2023-03-25 18:22:56 +0530

    added sample psycopg2 connection

---
+++ utils/db_connector.py
@@ -0,0 +1,21 @@
+import psycopg2
+
+#establishing the connection
+def db_connector():
+    conn = psycopg2.connect(
+    database="demo", user='param', password='helloworld', host='127.0.0.1', port= '5432'
+    )
+    #Creating a cursor object using the cursor() method
+    cursor = conn.cursor()
+
+    #Executing an MYSQL function using the execute() method 
+    #sample query to check if data is fetching fine 
+    cursor.execute("select * from indexooor")
+
+    # Fetch a single row using fetchone() method.
+    data = cursor.fetchall()
+    print("Connection established to: ",data)
+
+    #Closing the connection
+    
+        
